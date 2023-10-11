3f48fc184268e6838708b83a4b7a3388c722716c
Simonas Karuzas <simonas@not.cat>
2023-10-11 12:29:30 +0300

    feat: use remark

--- src/markdown.tsx
+++ src/markdown.tsx
@@ -2,31 +2,47 @@ import React from 'react'
 import { normalizeCredential } from 'did-jwt-vc';
 import { computeEntryHash } from '@veramo/utils';
 import { VerifiableCredentialComponent, CredentialLoader } from '@veramo-community/agent-explorer-plugin'
+import { Components } from 'react-markdown'
 
-import Token from 'markdown-it/lib/token';
+export const getMarkdownComponents = () : Partial<Components>  => {
 
-export const getMarkdownComponent = (token: Token)  => {
-
-  if (token.type === 'fence' && token.info === 'vc+jwt') {
-    const verifiableCredential = normalizeCredential(token.content.replace(/\s/g, ''))
-    const hash = computeEntryHash(verifiableCredential)
-    return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
-  }
-  if (token.type === 'fence' && token.info === 'vc+json') {
-    const verifiableCredential = JSON.parse(token.content)
-    const hash = computeEntryHash(verifiableCredential)
-    return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
-  } else if (token.type === 'fence' && token.info  === 'vc+multihash') {
-    const items = token.content.replace(/\s/g, '').split('/');
-    let hash = '';
-    let did = '';
-    if (items.length === 2) {
-      did = items[0];
-      hash = items[1];
-    } else {
-      hash = items[0];
+  return {
+    p(props) {
+      console.log(props)
+      return (<p>{JSON.stringify(props.node?.position)} {props.children}</p>)
+    },
+    pre(props) {
+      return (<>{props.children}</>)
+    },
+    code(props) {
+      const {children, className, node, ...rest} = props
+      switch (className?.toLowerCase()) {
+        case 'language-vc+jwt':
+          const verifiableCredential = normalizeCredential(String(children).replace(/\s/g, ''))
+          const hash = computeEntryHash(verifiableCredential)
+          return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
+        case 'language-vc+json':
+          const verifiableCredential2 = JSON.parse(String(children))
+          const hash2 = computeEntryHash(verifiableCredential2)
+          return <VerifiableCredentialComponent credential={{hash: hash2, verifiableCredential: verifiableCredential2}} />
+        case 'language-vc+multihash':
+          const items = String(children).replace(/\s/g, '').split('/');
+          let hash3 = '';
+          let did = '';
+          if (items.length === 2) {
+            did = items[0];
+            hash3 = items[1];
+          } else {
+            hash3 = items[0];
+          }
+          return <CredentialLoader hash={hash3} did={did} />
+        default:
+          return (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          )
+      }
     }
-
-    return <CredentialLoader hash={hash} did={did} />
   }
 }
